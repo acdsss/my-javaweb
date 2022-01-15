3908400b41b67cee12ea2c2c4bc017f82d868458
lvbingbing <githuawei@163.com>
2022-01-15 17:36:12 +0800

    测试

---
+++ src/main/java/test/TestDemo2.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author lvbingbing
+ * @date 2022-01-15 17:35
+ */
+public class TestDemo2 {
+    private static final Logger logger = Logger.getLogger(TestDemo2.class);
+    public static void main(String[] args) {
+        logger.info("hello java web");
+    }
+}
