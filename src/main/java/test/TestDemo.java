3041ee6d9a44b5eff64827fdc2acd889ec3645ca
liujianlong <2724530859@QQ.com>
2022-01-15 17:19:32 +0800

    initial commit test

---
+++ src/main/java/test/TestDemo.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author liujianlong
+ */
+public class TestDemo {
+
+    private static final Logger logger = Logger.getLogger(TestDemo.class);
+
+    public static void main(String[] args) {
+
+        logger.info("hello");
+    }
+}
